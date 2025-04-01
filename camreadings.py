8a802fb93b02cc6aebc6d7889ab4b0ba91fd9055
PrestonBodak <56452695+PrestonBodak@users.noreply.github.com>
2025-04-01 14:18:33 -0500

    Added console receiver files for sensors + cameras

---
+++ camreadings.py
@@ -0,0 +1,155 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from vision_msgs.msg import Detection2DArray
+from cv_bridge import CvBridge
+import cv2
+
+class CamSubscriber(Node):
+    def __init__(self):
+        super().__init__("intel_subscriber")
+
+        # Create subscriptions for RGB, Depth, and Object Detection topics from three cameras
+        self.subscription_rgb_1 = self.create_subscription(
+            Image, "camera_1/rgb_frame", self.rgb_frame_callback_1, 10
+        )
+        self.subscription_depth_1 = self.create_subscription(
+            Image, "camera_1/depth_frame", self.depth_frame_callback_1, 10
+        )
+        self.subscription_detections_1 = self.create_subscription(
+            Detection2DArray, "camera_1/detections", self.detection_callback_1, 10
+        )
+
+        self.subscription_rgb_2 = self.create_subscription(
+            Image, "camera_2/rgb_frame", self.rgb_frame_callback_2, 10
+        )
+        self.subscription_depth_2 = self.create_subscription(
+            Image, "camera_2/depth_frame", self.depth_frame_callback_2, 10
+        )
+        self.subscription_detections_2 = self.create_subscription(
+            Detection2DArray, "camera_2/detections", self.detection_callback_2, 10
+        )
+
+        self.subscription_rgb_3 = self.create_subscription(
+            Image, "camera_3/rgb_frame", self.rgb_frame_callback_3, 10
+        )
+        self.subscription_depth_3 = self.create_subscription(
+            Image, "camera_3/depth_frame", self.depth_frame_callback_3, 10
+        )
+        self.subscription_detections_3 = self.create_subscription(
+            Detection2DArray, "camera_3/detections", self.detection_callback_3, 10
+        )
+
+        # Initialize CvBridge to convert ROS images to OpenCV images
+        self.br = CvBridge()
+
+        # Variables to hold frames and detections
+        self.current_rgb_1 = None
+        self.current_depth_1 = None
+        self.current_detections_1 = []
+
+        self.current_rgb_2 = None
+        self.current_depth_2 = None
+        self.current_detections_2 = []
+
+        self.current_rgb_3 = None
+        self.current_depth_3 = None
+        self.current_detections_3 = []
+
+        # Set up a timer to regularly call the display function
+        self.timer = self.create_timer(0.1, self.timer_callback)  # 10 FPS, adjust if necessary
+
+    # RGB and Depth frame callbacks for Camera 1
+    def rgb_frame_callback_1(self, msg):
+        self.get_logger().info("Receiving Camera 1 RGB frame")
+        self.current_rgb_1 = self.br.imgmsg_to_cv2(msg)
+
+    def depth_frame_callback_1(self, msg):
+        self.get_logger().info("Receiving Camera 1 Depth frame")
+        self.current_depth_1 = self.br.imgmsg_to_cv2(msg)
+
+    def detection_callback_1(self, detections_msg):
+        self.get_logger().info("Receiving Camera 1 Object Detection data")
+        self.current_detections_1 = detections_msg.detections
+
+    # RGB and Depth frame callbacks for Camera 2
+    def rgb_frame_callback_2(self, msg):
+        self.get_logger().info("Receiving Camera 2 RGB frame")
+        self.current_rgb_2 = self.br.imgmsg_to_cv2(msg)
+
+    def depth_frame_callback_2(self, msg):
+        self.get_logger().info("Receiving Camera 2 Depth frame")
+        self.current_depth_2 = self.br.imgmsg_to_cv2(msg)
+
+    def detection_callback_2(self, detections_msg):
+        self.get_logger().info("Receiving Camera 2 Object Detection data")
+        self.current_detections_2 = detections_msg.detections
+
+    # RGB and Depth frame callbacks for Camera 3
+    def rgb_frame_callback_3(self, msg):
+        self.get_logger().info("Receiving Camera 3 RGB frame")
+        self.current_rgb_3 = self.br.imgmsg_to_cv2(msg)
+
+    def depth_frame_callback_3(self, msg):
+        self.get_logger().info("Receiving Camera 3 Depth frame")
+        self.current_depth_3 = self.br.imgmsg_to_cv2(msg)
+
+    def detection_callback_3(self, detections_msg):
+        self.get_logger().info("Receiving Camera 3 Object Detection data")
+        self.current_detections_3 = detections_msg.detections
+
+    def display_data(self):
+        """ Display the frames and detections for each camera """
+        if self.current_rgb_1 is not None:
+            for detection in self.current_detections_1:
+                x = int(detection.bbox.center.position.x)
+                y = int(detection.bbox.center.position.y)
+                w = int(detection.bbox.size_x)
+                h = int(detection.bbox.size_y)
+                cv2.rectangle(self.current_rgb_1, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                cv2.putText(self.current_rgb_1, f"{x},{y}", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+            cv2.imshow("Camera 1 RGB with Detection", self.current_rgb_1)
+
+        if self.current_rgb_2 is not None:
+            for detection in self.current_detections_2:
+                x = int(detection.bbox.center.position.x)
+                y = int(detection.bbox.center.position.y)
+                w = int(detection.bbox.size_x)
+                h = int(detection.bbox.size_y)
+                cv2.rectangle(self.current_rgb_2, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                cv2.putText(self.current_rgb_2, f"{x},{y}", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+            cv2.imshow("Camera 2 RGB with Detection", self.current_rgb_2)
+
+        if self.current_rgb_3 is not None:
+            for detection in self.current_detections_3:
+                x = int(detection.bbox.center.position.x)
+                y = int(detection.bbox.center.position.y)
+                w = int(detection.bbox.size_x)
+                h = int(detection.bbox.size_y)
+                cv2.rectangle(self.current_rgb_3, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                cv2.putText(self.current_rgb_3, f"{x},{y}", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+            cv2.imshow("Camera 3 RGB with Detection", self.current_rgb_3)
+
+        cv2.waitKey(1)
+
+    def timer_callback(self):
+        """ Regular callback to update the frames every time step """
+        self.display_data()
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    cam_subscriber = CamSubscriber()
+    rclpy.spin(cam_subscriber)
+    cam_subscriber.destroy_node()
+    rclpy.shutdown()
+
+    # Close all OpenCV windows when shutting down
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
--- subscriber_member_function.py
+++ subscriber_member_function.py
@@ -1,17 +1,3 @@
-# Copyright 2016 Open Source Robotics Foundation, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
@@ -20,102 +6,62 @@ from std_msgs.msg import Int32
 from std_msgs.msg import Int32MultiArray
 from std_msgs.msg import Float32MultiArray
 
-
 class MinimalSubscriber(Node):
 
     def __init__(self):
         super().__init__('minimal_subscriber')
 
-        print("Establishing QoS settings...")
+        print("Initializing...")
 
-        #match QoS to publisher
         qos_prof = QoSProfile(
             reliability = ReliabilityPolicy.BEST_EFFORT,
             durability = DurabilityPolicy.VOLATILE,
             depth = 10
         )
 
-        print("Initializing Teensy IMU 1 data subscription...")
+        print("Creating data subscriptions...")
 
         self.subscription = self.create_subscription(
-            Int32MultiArray,
-            'imu1_data',
-            self.imu_one_callback,
-            qos_profile = qos_prof)
-        self.subscription  # prevent unused variable warning
-
-        print("Initializing Teensy GPS data subscription...")
-
-        self.subscription = self.create_subscription(
-            Float32MultiArray,
-            'gps_data',
-            self.gps_callback,
-            qos_profile = qos_prof)
-        self.subscription  # prevent unused variable warning
-
-        print("Initializing Teensy IMU 2 data subscription...")
-
-        self.subscription = self.create_subscription(
-            Int32MultiArray,
-            'imu2_data',
-            self.imu_two_callback,
-            qos_profile = qos_prof)
-        self.subscription  # prevent unused variable warning
-
-        print("Initializing Teensy IMU 3 data subscription...")
-
-        self.subscription = self.create_subscription(
-            Int32MultiArray,
-            'imu3_data',
-            self.imu_three_callback,
-            qos_profile = qos_prof)
-        self.subscription  # prevent unused variable warning
-
-        #print("Initializing console data publisher")
-        
-        
-        self.imu1_publisher_ = self.create_publisher(
             Int32MultiArray,
             'console_imu1_data',
+            self.imu1_listener_callback,
             qos_profile = qos_prof)
+        self.subscription  # prevent unused variable warning
 
-        self.imu2_publisher_ = self.create_publisher(
+        self.subscription = self.create_subscription(
             Int32MultiArray,
             'console_imu2_data',
+            self.imu2_listener_callback,
             qos_profile = qos_prof)
+        self.subscription  # prevent unused variable warning
 
-        self.imu3_publisher_ = self.create_publisher(
+        self.subscription = self.create_subscription(
             Int32MultiArray,
             'console_imu3_data',
+            self.imu3_listener_callback,
             qos_profile = qos_prof)
+        self.subscription  # prevent unused variable warning
 
-        self.gps_publisher_ = self.create_publisher(
+        self.subscription = self.create_subscription(
             Float32MultiArray,
             'console_gps_data',
+            self.gps_listener_callback,
             qos_profile = qos_prof)
-        
-        
+        self.subscription  # prevent unused variable warning
 
+        print("Data subscription created!")
 
-    def imu_one_callback(self, msg):
-        self.get_logger().info('\nIMU 1 => ax: %s | ay: %s | az: %s | gx: %s | gy: %s | gz: %s' % (msg.data[0], msg.data[1], msg.data[2], msg.data[3], msg.data[4], msg.data[5]))
-        self.imu1_publisher_.publish(msg)
-        self.get_logger().info('Fowarding...')
+    def imu1_listener_callback(self, msg):
+        self.get_logger().info('IMU1 => I heard: "%s"' % msg.data[0])
+    
+    def imu2_listener_callback(self, msg):
+        self.get_logger().info('IMU2 => I heard: "%s"' % msg.data[0])
 
-    def imu_two_callback(self, msg):
-        self.get_logger().info('\nIMU 2 => ax: %s | ay: %s | az: %s | gx: %s | gy: %s | gz: %s' % (msg.data[0], msg.data[1], msg.data[2], msg.data[3], msg.data[4], msg.data[5]))
-        self.imu2_publisher_.publish(msg)
-        self.get_logger().info('Fowarding...')
+    def imu3_listener_callback(self, msg):
+        self.get_logger().info('IMU3 => I heard: "%s"' % msg.data[0])
 
-    def imu_three_callback(self, msg):
-        self.get_logger().info('\nIMU 3 => ax: %s | ay: %s | az: %s | gx: %s | gy: %s | gz: %s' % (msg.data[0], msg.data[1], msg.data[2], msg.data[3], msg.data[4], msg.data[5]))
-        self.imu3_publisher_.publish(msg)
-        self.get_logger().info('Fowarding...')
-
-    def gps_callback(self, msg):
-        self.get_logger().info('\nGPS => Latitude: %s | Longitude: %s' % (msg.data[0], msg.data[1]))
-        self.gps_publisher_.publish(msg)
-        self.get_logger().info('Fowarding...')       
+    def gps_listener_callback(self, msg):
+        self.get_logger().info('GPS => I heard: "%s"' % msg.data[0])
 
 
 def main(args=None):
@@ -123,7 +69,10 @@ def main(args=None):
 
     minimal_subscriber = MinimalSubscriber()
 
+    print("Spinning node...")
+
     rclpy.spin(minimal_subscriber)
+
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically

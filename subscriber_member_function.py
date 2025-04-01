eb23f039b2f3ba183db4c50873115f4f0a6197fa
PrestonBodak <56452695+PrestonBodak@users.noreply.github.com>
2025-04-01 14:09:15 -0500

    Update subscriber_member_function.py
    Compatibility with IMU + GPS measurements

--- subscriber_member_function.py
+++ subscriber_member_function.py
@@ -18,6 +18,7 @@ from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPo
 
 from std_msgs.msg import Int32
 from std_msgs.msg import Int32MultiArray
+from std_msgs.msg import Float32MultiArray
 
 
 class MinimalSubscriber(Node):
@@ -43,6 +44,15 @@ class MinimalSubscriber(Node):
             qos_profile = qos_prof)
         self.subscription  # prevent unused variable warning
 
+        print("Initializing Teensy GPS data subscription...")
+
+        self.subscription = self.create_subscription(
+            Float32MultiArray,
+            'gps_data',
+            self.gps_callback,
+            qos_profile = qos_prof)
+        self.subscription  # prevent unused variable warning
+
         print("Initializing Teensy IMU 2 data subscription...")
 
         self.subscription = self.create_subscription(
@@ -52,26 +62,60 @@ class MinimalSubscriber(Node):
             qos_profile = qos_prof)
         self.subscription  # prevent unused variable warning
 
+        print("Initializing Teensy IMU 3 data subscription...")
+
+        self.subscription = self.create_subscription(
+            Int32MultiArray,
+            'imu3_data',
+            self.imu_three_callback,
+            qos_profile = qos_prof)
+        self.subscription  # prevent unused variable warning
+
         #print("Initializing console data publisher")
         
-        '''
-        self.publisher_ = self.create_publisher(
+        
+        self.imu1_publisher_ = self.create_publisher(
             Int32MultiArray,
-            'console_data',
+            'console_imu1_data',
             qos_profile = qos_prof)
-        '''
+
+        self.imu2_publisher_ = self.create_publisher(
+            Int32MultiArray,
+            'console_imu2_data',
+            qos_profile = qos_prof)
+
+        self.imu3_publisher_ = self.create_publisher(
+            Int32MultiArray,
+            'console_imu3_data',
+            qos_profile = qos_prof)
+
+        self.gps_publisher_ = self.create_publisher(
+            Float32MultiArray,
+            'console_gps_data',
+            qos_profile = qos_prof)
+        
+        
 
 
     def imu_one_callback(self, msg):
         self.get_logger().info('\nIMU 1 => ax: %s | ay: %s | az: %s | gx: %s | gy: %s | gz: %s' % (msg.data[0], msg.data[1], msg.data[2], msg.data[3], msg.data[4], msg.data[5]))
-        #self.publisher_.publish(msg)
-        #self.get_logger().info('Fowarding...\n')
+        self.imu1_publisher_.publish(msg)
+        self.get_logger().info('Fowarding...')
 
     def imu_two_callback(self, msg):
         self.get_logger().info('\nIMU 2 => ax: %s | ay: %s | az: %s | gx: %s | gy: %s | gz: %s' % (msg.data[0], msg.data[1], msg.data[2], msg.data[3], msg.data[4], msg.data[5]))
-        #self.publisher_.publish(msg)
-        #self.get_logger().info('Fowarding...\n')
-        
+        self.imu2_publisher_.publish(msg)
+        self.get_logger().info('Fowarding...')
+
+    def imu_three_callback(self, msg):
+        self.get_logger().info('\nIMU 3 => ax: %s | ay: %s | az: %s | gx: %s | gy: %s | gz: %s' % (msg.data[0], msg.data[1], msg.data[2], msg.data[3], msg.data[4], msg.data[5]))
+        self.imu3_publisher_.publish(msg)
+        self.get_logger().info('Fowarding...')
+
+    def gps_callback(self, msg):
+        self.get_logger().info('\nGPS => Latitude: %s | Longitude: %s' % (msg.data[0], msg.data[1]))
+        self.gps_publisher_.publish(msg)
+        self.get_logger().info('Fowarding...')       
 
 
 def main(args=None):
